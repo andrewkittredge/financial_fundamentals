9b5760d25a7bad948a1ac00dfebef693b1a72ec1
akittredge <andrewlkittredge@gmail.com>
2013-03-27 22:06:05 -0400

    adding book_value_per_share

--- accounting_metrics.py
+++ accounting_metrics.py
@@ -10,28 +10,77 @@ gaap_namespaces = ('http://fasb.org/us-gaap/2011-01-31',
                    'http://xbrl.us/us-gaap/2009-01-31',
                    'http://fasb.org/us-gaap/2012-01-31')
 
+def value_from_filing(filing, element_of_interest):
+    for gaap_namespace in gaap_namespaces:
+        element_value = filing.findtext('{{{}}}{}'.format(gaap_namespace,
+                                                element_of_interest))
+        if element_value:
+            return float(element_value)
+    
     
 class EPS(object):
-    @staticmethod
-    def value_from_filing(filing):
-        for gaap_namespace in gaap_namespaces:
-            eps = filing.findtext('{{{}}}EarningsPerShareDiluted'.format(gaap_namespace))
-            if eps:
-                return float(eps)
-    
+    element_of_interest = 'EarningsPerShareDiluted'
+    @classmethod
+    def value_from_filing(cls, filing):
+        return value_from_filing(filing, cls.element_of_interest)
+
 class QuarterlyEPS(EPS):
     filing_type = '10-Q'
     metric_name = 'quarterly_eps'
     
 class AnnualEPS(EPS):
     filing_type = '10-K'
-        
 
+    
+class BookValuePerShare(object):
+    shares_outstanding_element = 'WeightedAverageNumberOfSharesOutstandingBasic'
+    @classmethod
+    def value_from_filing(cls, filing):
+        return cls._book_value(filing) / value_from_filing(filing, 
+                                                cls.shares_outstanding_element)
+    @classmethod
+    def _book_value(cls, filing):
+        assets = cls._assets(filing)
+        liabilities = cls._liabilities(filing)
+        return assets - liabilities
+    
+    assets_element = 'Assets'
+    @classmethod
+    def _assets(cls, filing):
+        return value_from_filing(filing, cls.assets_element)
+    
+    liabilities_element = 'Liabilities'    
+    @classmethod
+    def _liabilities(cls, filing):
+        return value_from_filing(filing, cls.liabilities_element)
+    
 class TestsXBRL(unittest.TestCase):
+    test_filing_path = 'test_docs/aapl-20121229.xml'
+    asset_test_value = 196088000000.
+    liabilities_test_value = 68742000000.
+    book_value_per_share_test_value = 135.6 # from http://www.gurufocus.com/term/Book%20Value%20Per%20Share/AAPL/Book%252BValue%252Bper%252BShare/Apple%2BInc
     def setUp(self):
         from xml.etree import cElementTree as ET
-        with open('test_docs/aapl-20121229.xml') as f:
-            self.appl_statement = ET.fromstring(f.read())
+        with open(self.test_filing_path) as f:
+            self.test_statement = ET.fromstring(f.read())
             
     def test_eps(self):
-        self.assertEqual(eps(self.appl_statement), 13.81)
+        quarterly_eps = QuarterlyEPS.value_from_filing(self.test_statement)
+        self.assertEqual(quarterly_eps, 13.81)
+        
+    def test_asssets(self):
+        self.assertEqual(BookValuePerShare._assets(self.test_statement), 
+                         self.asset_test_value)
+        
+    def test_liabilities(self):
+        self.assertEqual(BookValuePerShare._liabilities(self.test_statement), 
+                         self.liabilities_test_value)
+        
+    def test_book_value(self):
+        self.assertEqual(BookValuePerShare._book_value(self.test_statement),
+                         self.asset_test_value - self.liabilities_test_value)
+        
+    def test_book_value_per_share(self):
+        self.assertAlmostEqual(BookValuePerShare.value_from_filing(self.test_statement),
+                               self.book_value_per_share_test_value,
+                               delta=1.)
