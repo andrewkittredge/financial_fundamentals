29c47ef1af29c22e8148a219e517545a0f6a9bb6
akittredge <andrewlkittredge@gmail.com>
2013-08-13 21:05:07 -0400

    now you can use tickers for the CIK, removing find CIK logic

--- edgar.py
+++ edgar.py
@@ -23,30 +23,12 @@ ch.setLevel(logging.DEBUG)
 logger.addHandler(ch)
 
 
-
-def get_CIK(ticker):
-    search_url = 'http://www.sec.gov/cgi-bin/browse-edgar?company=&match=&CIK={}&filenum=&State=&Country=&SIC=&owner=exclude&Find=Find+Companies&action=getcompany'.format(ticker)
-    search_results_page = BeautifulSoup(requests.get(search_url).text)
-    try:
-        company_name = search_results_page.find('span', {'class' : 'companyName'}).a.text
-    except AttributeError:
-        if search_results_page.find('h1', text='No matching Ticker Symbol.'):
-            raise CouldNotFindCIK('Edgar cannot match {}'.format(ticker))
-        else:
-            raise
-    else:
-        cik = re.search('(\d+)', company_name).group()
-        return cik
-
-class CouldNotFindCIK(Exception):
-    pass
-
-SEARCH_URL = 'http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={cik}&type={filing_type}&dateb=&owner=exclude&count=100'
-def get_document_urls(cik, filing_type):
+SEARCH_URL = 'http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={symbol}&type={filing_type}&dateb=&owner=exclude&count=100'
+def get_document_urls(symbol, filing_type):
     '''Get the edgar filing_type document pages for the CIK.
     
     '''
-    search_url = SEARCH_URL.format(cik=cik, filing_type=filing_type)
+    search_url = SEARCH_URL.format(symbol=symbol, filing_type=filing_type)
     search_page = requests.get(search_url).text
     search_results_page = BeautifulSoup(search_page)
     xbrl_rows = [row for row in 
@@ -80,8 +62,7 @@ class NoFilingsFound(Exception):
 
 FILING_URLS = defaultdict(dict)
 def populate_filing_urls_map(ticker, filing_type, filing_url_map=FILING_URLS):
-    cik = get_CIK(ticker)
-    documents_urls = get_document_urls(cik, filing_type)
+    documents_urls = get_document_urls(symbol=ticker, filing_type=filing_type)
     find_urls_on_search_page(documents_urls, ticker, filing_type, filing_url_map)
         
 class XBRLNotAvailable(Exception):
@@ -114,10 +95,7 @@ class TestsEdgar(unittest.TestCase):
     def setUp(self):
         import requests_cache
         requests_cache.configure('fundamentals_cache_test')
-    
-    def test_get_CIK(self):
-        apple_CIK = get_CIK('aapl')
-        self.assertEqual(apple_CIK, '0000320193')
+
         
     def test_populate_filing_url_map(self):
         test_map = defaultdict(dict)
@@ -158,9 +136,8 @@ class TestsEdgar(unittest.TestCase):
         with open('test_docs/abbv_search_results.html') as test_html:
             text.return_value = test_html.read()
 
-        abbv_CIK = '0001551152'
-        self.assertFalse(list(get_document_urls(cik=abbv_CIK, filing_type='10-Q')))
         ticker = 'ABBV'
+        self.assertFalse(list(get_document_urls(symbol=ticker, filing_type='10-Q')))
         filing_type = '10-Q'
         date_after = date(2013, 1, 2)
         
@@ -170,20 +147,6 @@ class TestsEdgar(unittest.TestCase):
                                                   filing_map=defaultdict(dict))
         self.assertRaises(NoFilingFound, finds_no_filings)
 
-    def test_PDCO_CIK(self):
-        '''This failed.
-        
-        '''
-        ticker = 'PDCO'
-        cik = get_CIK(ticker)
-        self.assertEqual(cik, '0000891024')
-        
-    def test_no_matching_ticker(self):
-        '''SEC doesn't know about PPL for some reason.
-        
-        '''
-        throws_missing_ticker = lambda : get_CIK('PPL')
-        self.assertRaises(CouldNotFindCIK, throws_missing_ticker)
-        
 if __name__ == '__main__':
-    print list(get_document_urls(cik='0001551152', filing_type='10-Q'))
+    import datetime
+    print filing_before(ticker='GOOG', filing_type='10-Q', date_after=datetime.date(2012, 12, 1))
