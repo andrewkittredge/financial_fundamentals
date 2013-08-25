4e9b8090f35eebb051f5f11c38abf68f0bf4c856
akittredge <andrewlkittredge@gmail.com>
2013-08-24 22:03:55 -0400

    untested setup file

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(name='financial_fundamentals',
+      version='1',
+      description='Caching for financial metrics.',
+      author='Andrew Kittredge',
+      author_email='andrewlkittredge@gmail.com',
+      license='Apache 2.0',
+      classifiers=[
+	'Development Status :: 4 - Beta',
+	'License :: OSI Approved :: Apache Software License',
+	'Natural Language :: English',
+	'Programming Language :: Python',
+	'Programming Language :: Python :: 2.7',
+	'Operating Audience :: Science/Research',
+	'Intended Audience :: Science/Research',
+	'Topic :: Office/Business :: Financial',
+     	],	
+      install_requires=[
+	'numpy',
+	'pytz',
+	'request_cache',
+	'request',
+	'BeautifulSoup',
+	'pymongo',
+	'mock',
+	'pandas',
+	],
+      url='https://github.com/andrewkittredge/financial_fundamentals',
+)
