94f2008667756ecbafb5bd45d859409f6de2078b
akittredge <andrewlkittredge@gmail.com>
2013-09-05 19:55:56 -0400

    polishing setup

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 from setuptools import setup, find_packages
 
+version = '0.1.1'
 desc = '''\
-financial_fundamentals
+FinancialFundamentals
 =========================
 :Author: Andrew Kittredge
-:Version: $Revision: 0.1.0
+:Version: $Revision: {}
 :Copyright: Andrew Kittredge
 :License: Apache Version 2
 
 
-financial_fundamentals caches financial data to speed alogirithm development.  It is developed with the zipline backtesting library in mind. Currently it caches prices downloaded from yahoo and two accounting metrics extracted from XBRL downloaded from the SEC's Edgar system.
+FinancialFundamentals caches financial data to speed alogirithm development.  It is developed with the zipline backtesting library in mind. Currently it caches prices downloaded from yahoo and two accounting metrics extracted from XBRL downloaded from the SEC's Edgar system.
 
+FinancialFundamentals is under active development, comments, suggestions, and bug reports are appreciated.
 
-'''
+'''.format(version)
+
 setup(name='FinancialFundamentals',
-      version='0.1.0',
+      version=version,
       description='Caching for financial metrics.',
       long_description=desc,
       author='Andrew Kittredge',
@@ -27,7 +30,7 @@ setup(name='FinancialFundamentals',
 	'Natural Language :: English',
 	'Programming Language :: Python',
 	'Programming Language :: Python :: 2.7',
-	'Operating Audience :: Science/Research',
+	'Operating System :: OS Independent',
 	'Intended Audience :: Science/Research',
 	'Topic :: Office/Business :: Financial',
      	],	
