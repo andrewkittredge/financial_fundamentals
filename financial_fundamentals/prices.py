e28a711fa52254653e580b3bd53fc5e70def9d79
akittredge <andrewlkittredge@gmail.com>
2013-09-08 15:25:22 -0400

    don't look for prices yahoo doesn't have

--- financial_fundamentals/prices.py
+++ financial_fundamentals/prices.py
@@ -9,9 +9,19 @@ from pandas.io.data import get_data_yahoo
 import datetime
 import numpy as np
 import pytz
+from financial_fundamentals.exceptions import NoDataForStock
+
+SYMBOLS_YAHOO_DOES_NOT_HAVE = {'CVH',
+                               'HNZ',
+                               'PCS',
+                               'S',
+                               }
+
 
 def get_prices_from_yahoo(symbol, dates, type_of_price='Adj Close'):
     '''Yields date, value pairs.'''
+    if symbol in SYMBOLS_YAHOO_DOES_NOT_HAVE:
+        raise NoDataForStock("Cannot download {} from yahoo".format(symbol))
     start = min(dates)
     end = max(dates)
     prices = get_data_yahoo(name=symbol, start=start, end=end)
