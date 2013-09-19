51b30d535f0f23ef09ac2c7cc65679049a009855
akittredge <andrewlkittredge@gmail.com>
2013-09-18 23:10:51 -0400

    adding index for performance.

--- financial_fundamentals/sqlite_drivers.py
+++ financial_fundamentals/sqlite_drivers.py
@@ -5,7 +5,6 @@ Created on Aug 21, 2013
 '''
 
 import numpy as np
-import dateutil.parser
 import sqlite3
 
 import pytz
@@ -18,31 +17,38 @@ class SQLiteDriver(object):
         self._table = table
         self._metric = metric
         self._ensure_table_exists(connection, table)
-        
+
     @classmethod
     def _ensure_table_exists(cls, connection, table):
-        cursor = connection.cursor()
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-        tables = [row['name'] for row in cursor.fetchall()]
-        if table not in tables:
-            cls.create_table(connection, table)
-            
-    @classmethod
-    def create_table(cls, connection, table):
         with connection:
-            connection.execute(cls.create_stm.format(table))
+            cursor = connection.cursor()
+            cursor.execute(cls._create_stmt.format(table_name=table))
             
 class SQLiteTimeseries(SQLiteDriver):
-    create_stm = 'CREATE TABLE {:s} (date timestamp, symbol text, metric text, value real)'
-    get_query = '''SELECT value, symbol, metric, date from {} 
+    _create_stmt = '''CREATE TABLE IF NOT EXISTS {table_name}
+                    (date timestamp, symbol text, metric text, value real);
+                 '''
+    _create_index_stmt = '''
+                        CREATE INDEX IF NOT EXISTS 
+                        time_series_index ON {table_name} (date, symbol, metric);
+                        '''
+    _get_query = '''SELECT value, date from {} 
                     WHERE symbol = ?
                     AND date in ({})
                     AND metric =  ?
                 '''
+    @classmethod
+    def _ensure_table_exists(cls, connection, table):
+        super(cls, SQLiteTimeseries)._ensure_table_exists(connection,
+                                                          table)
+        with connection:
+            cursor = connection.cursor()
+            cursor.execute(cls._create_index_stmt.format(table_name=table))
+            
     def get(self, symbol, dates):
         '''return metric values for symbols and dates.'''
         with self._connection:
-            qry = self.get_query.format(self._table, 
+            qry = self._get_query.format(self._table, 
                                         ','.join('?' * len(dates)),
                                         )
             cursor = self._connection.cursor()
@@ -64,11 +70,19 @@ class SQLiteTimeseries(SQLiteDriver):
                 self._connection.execute(self.insert_query.format(self._table), args)
     
 class SQLiteIntervalseries(SQLiteDriver):
-    create_stm = 'CREATE TABLE {:s} (start timestamp, end timestamp, symbol text, metric text, value real)'
-    get_qry = 'SELECT value FROM {} WHERE metric = ? AND symbol = ? AND start <= ? AND ? <= end'
+    _create_stmt = '''CREATE TABLE {table_name} 
+                        (start timestamp, 
+                        end timestamp, 
+                        symbol text, 
+                        metric text, 
+                        value real)
+                    '''
+    _get_qry = '''SELECT value FROM {} 
+                    WHERE metric = ? AND symbol = ? AND start <= ? AND ? <= end
+                    '''
     def get(self, symbol, date):
         '''return the metric value of symbol on date.'''
-        qry = self.get_qry.format(self._table)
+        qry = self._get_qry.format(self._table)
         with self._connection:
             row = self._connection.execute(qry, (self._metric,
                                                  symbol,
@@ -76,10 +90,12 @@ class SQLiteIntervalseries(SQLiteDriver):
                                                  date)).fetchone()
         return row and (np.float(row['value']) if row['value'] else np.NaN)
 
-    insert_query = 'INSERT INTO {} (symbol, start, end, metric, value) VALUES (?, ?, ?, ?, ?)'
+    _insert_query = '''INSERT INTO {} 
+                        (symbol, start, end, metric, value) VALUES (?, ?, ?, ?, ?)
+                        '''
     def set_interval(self, symbol, start, end, value):
         '''set value for interval start and end.'''
-        qry = self.insert_query.format(self._table)
+        qry = self._insert_query.format(self._table)
         with self._connection:
             self._connection.execute(qry, (symbol, start, end, 
                                            self._metric, value))
