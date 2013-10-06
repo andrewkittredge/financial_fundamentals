fac69892dca42301c063704f9981bf474018fc49
akittredge <andrewlkittredge@gmail.com>
2013-10-06 11:57:36 -0400

    re-working edgar driver

--- financial_fundamentals/edgar.py
+++ financial_fundamentals/edgar.py
@@ -7,50 +7,86 @@ import requests
 from BeautifulSoup import BeautifulSoup
 import re
 from datetime import date
-
-
-from xml.etree import cElementTree as ET
-import logging
 from urlparse import urljoin
-from collections import defaultdict
-from bisect import bisect_left
-from dateutil.relativedelta import relativedelta
-from financial_fundamentals.exceptions import NoDataForStock
-
-ticker_search_string = 'http://www.sec.gov/cgi-bin/browse-edgar?company=&match=&CIK={}&filenum=&State=&Country=&SIC=&owner=exclude&Find=Find+Companies&action=getcompany'
-
-logger = logging.getLogger('edgar')
-logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-logger.addHandler(ch)
+import blist
 
 
-SEARCH_URL = 'http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={symbol}&type={filing_type}&dateb=&owner=exclude&count=100'
-def get_document_urls(symbol, filing_type):
-    '''Get the edgar filing_type document pages for the CIK.
-    
-    '''
-    search_url = SEARCH_URL.format(symbol=symbol, filing_type=filing_type)
-    search_page = requests.get(search_url).text
-    search_results_page = BeautifulSoup(search_page)
-    xbrl_rows = [row for row in 
-                 search_results_page.findAll('tr') if 
-                 row.find(text=re.compile('Interactive Data'))]
-    for xbrl_row in xbrl_rows:
-        documents_page = xbrl_row.find('a', {'id' : 'documentsbutton'})['href']
-        documents_url = 'http://sec.gov' + documents_page
-        yield documents_url
-    
+SEARCH_URL = 'http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={symbol}&type={filing_type}&dateb=&owner=exclude&count=100'        
+class XBRLNotAvailable(Exception):
+    pass
 
-def find_urls_on_search_page(documents_urls, ticker, filing_type, filing_url_map):
-    for documents_url in documents_urls:
-        filing_page = BeautifulSoup(requests.get(documents_url).text)
+class Filing(object):
+    '''wrap filings.'''
+    def __init__(self, filing_date, xbrl_url=None):
+        self.xbrl_url = xbrl_url
+        self.filing_date = filing_date
+
+    def __cmp__(self, other):
+        return (self.filing_date - other.filing_date).days
+
+
+
+class EdgarHTMLDriver(object):
+    '''Get documents from Edgar by parsing the HTML.'''
+    _ticker_filings = {}
+    @classmethod
+    def get_filing(cls, ticker, filing_type, date_after):
+        '''Get the last xbrl filed before date.
+            Returns a Filing object, return None if there are no XBRL documents
+            prior to the date.
+        '''
+        filings = cls._ticker_filings.setdefault(ticker,
+                                                 cls._get_filing_urls(ticker, 
+                                                                      filing_type)
+                                                 )
+        if filings:
+            search_filing = Filing(filing_date=date_after) # hack filing for bisecting
+            filing_before_index = filings.bisect_right(search_filing) - 1
+            if filing_before_index == -1:
+                raise XBRLNotAvailable('date is before the first XBRL filing in Edgar.')
+            return filings[filing_before_index]
+        else:
+            raise XBRLNotAvailable('No XBRL filings found.')
+
+    @classmethod
+    def _get_filing_urls(cls, ticker, filing_type):
+        '''Step 1 Search for the ticker and filing type,
+            generate the urls for the document pages that have interactive data/XBRL.
+        Step 2 : Get the document pages, on each page find the url for the XBRL document.
+            Return a blist sorted by filing date.
+        '''
+        filings = blist.sortedlist()
+        document_page_urls = cls._get_document_page_urls(ticker, filing_type)
+        for url in document_page_urls:
+            filing = cls._get_filing_from_document_page(url)
+            filings.add(filing)
+        return filings
+
+    @classmethod
+    def _get_document_page_urls(cls, symbol, filing_type):
+        '''Get the edgar filing document pages for the CIK.
+        
+        '''
+        search_url = SEARCH_URL.format(symbol=symbol, filing_type=filing_type)
+        search_page = requests.get(search_url).text
+        search_results_page = BeautifulSoup(search_page)
+        xbrl_rows = [row for row in 
+                     search_results_page.findAll('tr') if 
+                     row.find(text=re.compile('Interactive Data'))]
+        for xbrl_row in xbrl_rows:
+            documents_page = xbrl_row.find('a', {'id' : 'documentsbutton'})['href']
+            documents_url = 'http://sec.gov' + documents_page
+            yield documents_url
+
+    @classmethod
+    def _get_filing_from_document_page(cls, document_page_url):
+        '''Find the XBRL link on a page like 
+        http://www.sec.gov/Archives/edgar/data/320193/000119312513300670/0001193125-13-300670-index.htm
+        
+        '''
+        filing_page = BeautifulSoup(requests.get(document_page_url).text)
         period_of_report_elem = filing_page.find('div', text='Filing Date')
-        try:
-            filing_date = period_of_report_elem.findNext('div', {'class' : 'info'}).text
-        except AttributeError:
-            continue
+        filing_date = period_of_report_elem.findNext('div', {'class' : 'info'}).text
         filing_date = date(*map(int, filing_date.split('-')))
         type_tds = filing_page.findAll('td', text='EX-101.INS')
         for type_td in type_tds:
@@ -60,59 +96,10 @@ def find_urls_on_search_page(documents_urls, ticker, filing_type, filing_url_map
                 continue
             else:
                 break
-        filing_xbrl_url = urljoin('http://www.sec.gov', xbrl_link)
-        filing_url_map[ticker][(filing_type, filing_date)] = filing_xbrl_url
-
-class NoFilingsFound(Exception):
-    pass
-
-FILING_URLS = defaultdict(dict)
-def populate_filing_urls_map(ticker, filing_type, filing_url_map=FILING_URLS):
-    documents_urls = get_document_urls(symbol=ticker, filing_type=filing_type)
-    find_urls_on_search_page(documents_urls, ticker, filing_type, filing_url_map)
-        
-class XBRLNotAvailable(Exception):
-    pass
-
-def _filing_url_before(ticker, filing_type, date_after, filing_map=FILING_URLS):
-    '''Return the url for the XBRL bracketed by the dates it was 'in effect.'
-    We want the filing that was submitted before the date_after, what you would have been
-    trading based on.
-    
-    '''
-    if ticker not in filing_map:
-        populate_filing_urls_map(ticker, filing_type, filing_map)
-    filing_dates = sorted(key[1] for key in filing_map[ticker])
-    if not filing_dates:
-        raise XBRLNotAvailable('No {}s found for {}'.format(filing_type, ticker))
-    filing_date_index = bisect_left(filing_dates, date_after) - 1
-    filing_date_before_date_requested = filing_dates[filing_date_index]
-    if filing_date_index < len(filing_dates) - 1:
-        filing_date_after_that = filing_dates[filing_date_index + 1]
-    else:
-        #Needless to say this is hack.  We could get the last non-xbrl filing date
-        # from edgar.
-        filing_date_after_that = filing_date_before_date_requested + relativedelta(months=3)
-        
-    return (filing_date_before_date_requested, 
-            filing_map[ticker][(filing_type, filing_date_before_date_requested)], 
-            filing_date_after_that)
-
-
-def filing_before(ticker, filing_type, date_after, filing_map=FILING_URLS):
-    try:
-        interval_start, filing_url, interval_end = _filing_url_before(ticker, 
-                                                                      filing_type, 
-                                                                      date_after, 
-                                                                      filing_map)
-    except XBRLNotAvailable:
-        raise NoFilingFound('No filing found for ticker {}'.format(ticker))
-    filing_text = ET.fromstring(requests.get(filing_url).text)
-    return interval_start, filing_text, interval_end
-
-class NoFilingFound(NoDataForStock):
-    '''No filings on Edgar for this stock.'''
+        xbrl_url = urljoin('http://www.sec.gov', xbrl_link)
+        filing = Filing(filing_date=filing_date, xbrl_url=xbrl_url)
+        return filing 
 
 if __name__ == '__main__':
     import datetime
-    print filing_before(ticker='GOOG', filing_type='10-Q', date_after=datetime.date(2012, 12, 1))
+    print EdgarHTMLDriver.get_filing(ticker='GOOG', filing_type='10-Q', date_after=datetime.date(1960, 9, 29)).xbrl_url
--- tests/test_edgar.py
+++ tests/test_edgar.py
@@ -6,50 +6,31 @@ Created on Sep 12, 2013
 
 import mock
 import unittest
-from financial_fundamentals.edgar import populate_filing_urls_map,\
-    _filing_url_before, get_document_urls, filing_before, NoFilingFound
-from collections import defaultdict
 from datetime import date
 from tests.infrastructure import turn_on_request_caching, TEST_DOCS_DIR
 import os
+from financial_fundamentals.edgar import EdgarHTMLDriver, XBRLNotAvailable
 
 class TestsEdgar(unittest.TestCase):
     def setUp(self):
         turn_on_request_caching()
 
-    def test_populate_filing_url_map(self):
-        test_map = defaultdict(dict)
-        ticker = 'aapl'
-        filing_type = '10-Q'
-        populate_filing_urls_map(ticker=ticker, 
-                                 filing_type=filing_type, 
-                                 filing_url_map=test_map)
-        self.assertEqual(test_map[ticker][(filing_type, date(2013, 1, 24))], 
+    def test_get_filiing(self):
+        filing = EdgarHTMLDriver.get_filing(ticker='aapl', 
+                                            filing_type='10-Q',
+                                            date_after=date(2013, 1, 24)
+                                            )
+        self.assertEqual(filing.xbrl_url, 
                          'http://www.sec.gov/Archives/edgar/data/320193/000119312513022339/aapl-20121229.xml')
-    
-    def test_last_filing_before(self):
-        test_map = defaultdict(dict)
-        ticker = 'aapl'
-        filing_type = '10-Q'
-        populate_filing_urls_map(ticker,
-                                 filing_type=filing_type,
-                                 filing_url_map=test_map)
-        date_after = date(2010, 7, 22)
-        june_2010_filing_url = 'http://www.sec.gov/Archives/edgar/data/320193/000119312510162840/aapl-20100626.xml'
-        interval_start, filing, interval_end = _filing_url_before(ticker, filing_type, date_after, filing_map=test_map)
-        self.assertEqual(filing, june_2010_filing_url)
-        self.assertEqual(interval_start, date(2010, 7, 21))
-        self.assertEqual(interval_end, date(2011, 1, 19))
 
     def test_mmm(self):
         '''This was getting a text file instead of xml.
         
         '''
-        _, filing_url, _ = _filing_url_before(ticker='MMM', 
-                                              filing_type='10-Q',
-                                              date_after=date(2010, 1, 04), 
-                                              filing_map=defaultdict(dict))
-        self.assertTrue(filing_url.endswith('.xml'))
+        filing = EdgarHTMLDriver.get_filing(ticker='MMM', 
+                                                filing_type='10-Q',
+                                                date_after=date(2010, 1, 04))
+        self.assertTrue(filing.xbrl_url.endswith('.xml'))
         
     @mock.patch('requests.models.Response.text', new_callable=mock.PropertyMock)
     def test_ABBV(self, text):
@@ -61,12 +42,18 @@ class TestsEdgar(unittest.TestCase):
             text.return_value = test_html.read()
 
         ticker = 'ABBV'
-        self.assertFalse(list(get_document_urls(symbol=ticker, filing_type='10-Q')))
+        self.assertFalse(list(EdgarHTMLDriver._get_filing_urls(ticker=ticker, 
+                                                               filing_type='10-Q')
+                              )
+                         )
         filing_type = '10-Q'
         date_after = date(2013, 1, 2)
         
-        finds_no_filings = lambda : filing_before(ticker, 
-                                                  filing_type, 
-                                                  date_after, 
-                                                  filing_map=defaultdict(dict))
-        self.assertRaises(NoFilingFound, finds_no_filings)
+        finds_no_filings = lambda : EdgarHTMLDriver.get_filing(ticker, 
+                                                               filing_type, 
+                                                               date_after, 
+                                                               )
+        self.assertRaises(XBRLNotAvailable, finds_no_filings)
+
+    def test_sort_order(self):
+        pass
