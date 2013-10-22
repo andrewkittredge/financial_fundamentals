06cd827c324c1d8bcbbe64e386de747b33f77bd6
akittredge <andrewlkittredge@gmail.com>
2013-10-21 22:58:26 -0400

    new exception

--- financial_fundamentals/exceptions.py
+++ financial_fundamentals/exceptions.py
@@ -7,5 +7,9 @@ Created on Sep 8, 2013
 class NoDataForStock(Exception):
     '''Raise when the metric is not available for the stock.'''
     
+class NoDataForStockOnDate(Exception):
+    '''Raised when the metric is not available fo the stock on the requested date.'''
+    
 class ExternalRequestFailed(Exception):
     '''Raised when a call to an external service fails.'''
+    
